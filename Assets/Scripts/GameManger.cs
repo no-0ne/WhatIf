7b04bc57d4feb41dda23d66122de3777f8673d08
no-0ne <no-0ne@github.com>
2016-10-22 02:03:08 +0400

    👾

---
+++ Assets/Scripts/GameManger.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class GameManger : MonoBehaviour
+{
+    public GameObject StartScreen;
+    public GameObject MainScreen;
+    public GameObject EndScreen;
+
+    public int Iron;
+    public int SpaceShip;
+
+    private GameObject currentScreen;
+
+    public static GameManger Instance;
+
+    void Start()
+    {
+        Instance = this;
+        Show(StartScreen);
+    }
+
+    public void StartGame()
+    {
+        Show(MainScreen);
+    }
+
+    public void EndGame()
+    {
+        Show(EndScreen);
+    }
+
+    public void GetIron()
+    {
+
+    }
+
+    public void MakeSpaceShip()
+    {
+
+    }
+
+    void Show(GameObject screen)
+    {
+        if (currentScreen != null)
+        {
+            Destroy(currentScreen);
+        }
+        currentScreen = (GameObject)Instantiate(screen, transform, false);
+    }
+}
---
+++ Assets/Scripts/MainScreen.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class MainScreen : MonoBehaviour {
+
+    public void GetIron()
+    {
+        GameManger.Instance.GetIron();
+    }
+
+    public void MakeSpaceShip()
+    {
+        GameManger.Instance.MakeSpaceShip();
+    }
+}
